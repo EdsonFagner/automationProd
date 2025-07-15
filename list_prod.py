79ce3abece0483a521710f7ed0c6e3b076bba9ad
decorj15 <edson.fagner.souto@gmail.com>
2025-07-14 23:00:33 -0300

    create some screens

---
+++ list_prod.py
@@ -0,0 +1,27 @@
+import simple_screen
+import panel_screen
+
+def click_button_register():
+    print('register')
+
+def click_button_update():
+    print('update')
+
+def click_button_delete():
+    print('delete')
+
+def click_button_return():
+    simple_screen.window.destroy()
+    panel_screen.panel_screen()
+
+def prod_list_screen():
+    buttons_list = []
+    buttons_list.append(simple_screen.ArrayButton('CADASTRAR', 'DodgerBlue2', 'SteelBlue2', click_button_register, 'r-user'))
+    buttons_list.append(simple_screen.ArrayButton('ATUALIZAR', 'DodgerBlue2', 'SteelBlue2', click_button_update, 'u-user'))
+    buttons_list.append(simple_screen.ArrayButton('EXCLUIR', 'DodgerBlue2', 'SteelBlue2', click_button_delete, 'd-user'))
+    buttons_list.append(simple_screen.ArrayButton('VOLTAR', 'red', 'red4', click_button_return, 'return'))
+
+    simple_screen.create_screen('500x300', 'Cadastro de Usuários', buttons_list)
+
+if __name__ == '__main__':
+    prod_list_screen()
--- panel_screen.py
+++ panel_screen.py
@@ -1,10 +1,14 @@
 import simple_screen
+import register_user
+import list_prod
 
 def register_users():
-    print('users')
+    simple_screen.window.destroy()
+    register_user.register_user_screen()
 
 def register_prodution_list():
-    print('prodution')
+    simple_screen.window.destroy()
+    list_prod.prod_list_screen()
 
 def show_prod_list():
     print('prodution')
@@ -13,16 +17,14 @@ def close_window():
     simple_screen.window.destroy()
 
 
-def panel_secreen():
+def panel_screen():
     buttons_list = []
     buttons_list.append(simple_screen.ArrayButton('CADASTRO DE USUÁRIOS', 'DodgerBlue2', 'SteelBlue2', register_users, 'c-users'))
     buttons_list.append(simple_screen.ArrayButton('CADASTRO DE LISTA DE PRODUÇÃO', 'DodgerBlue2', 'SteelBlue2', register_prodution_list, 'c-prodution'))
     buttons_list.append(simple_screen.ArrayButton('EXIBIR LISTAS', 'DodgerBlue2', 'SteelBlue2', show_prod_list, 'show-prodution'))
     buttons_list.append(simple_screen.ArrayButton('SAIR', 'red', 'red4', close_window, 'exit'))
 
-
-
     simple_screen.create_screen('500x300', 'Painel de Controle', buttons_list)
 
 if __name__ == '__main__':
-    panel_secreen()
+    panel_screen()
